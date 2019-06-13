0fd0d3b638124cfbb38f72d6bb280f00c721ce5e
Victor Longo <victor.m.longo@gmail.com>
2019-06-13 16:46:17 -0300

    Modulo suscriptores terminado, empezando modulo libros

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,14 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import store from '../store';
 import { Provider } from 'react-redux';
 
+import Libros from './libros/Libros';
+import MostrarLibro from './libros/MostrarLibro';
+import NuevoLibro from './libros/NuevoLibro';
+import EditarLibro from './libros/EditarLibro';
+import PrestamoLibro from './libros/PrestamoLibro';
+
+
+
 import Suscriptores from "./suscriptores/Suscriptores";
 import MostrarSuscriptor from "./suscriptores/MostrarSuscriptor";
 import NuevoSuscriptor from "./suscriptores/NuevoSuscriptor";
@@ -17,6 +25,14 @@ function App() {
 
         <div className="container">
           <Switch>
+            <Route exact path="/" component={Libros} />
+            <Route exact path="/libros/mostrar/:id" component={MostrarLibro} />
+            <Route exact path="/libros/nuevo" component={NuevoLibro} />
+            <Route exact path="/libros/editar/:id" component={EditarLibro} />
+            <Route exact path="/libros/prestamo/:id" component={PrestamoLibro} />
+
+
+
             <Route exact path="/suscriptores" component={Suscriptores} />
             <Route exact path="/suscriptores/nuevo" component={NuevoSuscriptor} />
             <Route exact path="/suscriptores/mostrar/:id" component={MostrarSuscriptor} />
---
+++ src/components/libros/PrestamoLibro.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PrestamoLibro extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Prestamos libros</h1>
+            </div>
+        );
+    }
+}
+
+export default PrestamoLibro;
